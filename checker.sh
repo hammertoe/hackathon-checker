f9c83d32aa3e8605cbbfeb5c63677e10c06fed8a
Matt Hamilton <matt@dharach.com>
2024-04-11 13:54:24 -0400

    also print out the ecosystem this is part of: filecoin, fvm, ipfs

--- checker.sh
+++ checker.sh
@@ -7,6 +7,7 @@ fi
 
 # Start with empty list of tags
 tags=""
+ecosystem=""
 
 # Extract the repository name from the url
 repo_name=$(basename "$1" .git)
@@ -33,55 +34,63 @@ all_files=$(find . \( -type d -name "node_modules" -prune \) -o -type f -print)
 # Find all Solidity files
 sol_files=$(echo "$all_files" | egrep "\.sol$")
 if [ -n "$sol_files" ]; then
-  tags="${tags} solidity"
+    tags="${tags} solidity"
 fi
 
 # Find all JS files
 js_files=$(echo "$all_files" | egrep "\.(js|ts)$")
 if [ -n "$js_files" ]; then
-  tags="${tags} javascript"
+    tags="${tags} javascript"
 fi
 
 # Find all Hardhat config files
 hh_files=$(echo "$js_files" | grep "hardhat.config.")
 if [ -n "$hh_files" ]; then
-  tags="${tags} hardhat"
+    tags="${tags} hardhat"
 fi
 
 # Search for usage of the filecoin solidity library
 fs_usage=$(echo "$sol_files" | tr '\n' '\0' | xargs -P0 -0 -I {} grep 'import.*\(MarketAPI\|DataCapAPI\|MinerAPI\|SendAPI\|VerifRegAPI\|PowerAPI\)' "{}")
 if [ -n "$fs_usage" ]; then
-  tags="${tags} filecoin-sol"
+    tags="${tags} filecoin-sol"
+    ecosystem="${ecosystem} filecoin"
 fi
 
 # Search for usage of the hyperspace endpoint
 hs_usage=$(echo "$js_files" | tr '\n' '\0' | xargs -P0 -0 -I {} grep '\(https://filecoin-hyperspace.chainstacklabs.com/rpc/v1\|https://api.hyperspace.node.glif.io/rpc/v1\)' "{}")
 if [ -n "$hs_usage" ]; then
-  tags="${tags} hyperspace"
+    tags="${tags} hyperspace"
+    ecosystem="${ecosystem} filecoin fvm"    
 fi
 
 # Search for usage of the calibration endpoint
 cal_usage=$(echo "$js_files" | tr '\n' '\0' | xargs -P0 -0 -I {} grep '\(https://api.calibration.node.glif.io/rpc/v1\|https://filecoin-calibration.chainup.net/rpc/v1\|https://rpc.ankr.com/filecoin_testnet\|https://calibration.filfox.info/rpc/v1\)' "{}")
 if [ -n "$cal_usage" ]; then
-  tags="${tags} calibration"
+    tags="${tags} calibration"
+    ecosystem="${ecosystem} filecoin fvm"    
 fi
 
 # Search for usage of Lighthouse
 lh_usage=$(echo "$js_files" | tr '\n' '\0' | xargs -P0 -0 -I {} grep 'lighthouse-web3/sdk' "{}")
 if [ -n "$lh_usage" ]; then
   tags="${tags} lighthouse"
+  ecosystem="${ecosystem} filecoin ipfs"    
 fi
 
 # Search for usage of Web3 Storage
 w3_usage=$(echo "$js_files" | tr '\n' '\0' | xargs -P0 -0 -I {} grep 'import.*\(web3-storage/w3up-client\|web3.storage\)' "{}")
 if [ -n "$w3_usage" ]; then
-  tags="${tags} web3storage"
+    tags="${tags} web3storage"
+    ecosystem="${ecosystem} filecoin ipfs"
 fi
 
 # Search for usage of NFT Storage
 nft_usage=$(echo "$js_files" | tr '\n' '\0' | xargs -P0 -0 -I {} grep 'import.*\(nft.storage\)' "{}")
 if [ -n "$nft_usage" ]; then
-  tags="${tags} nftstorage"
+    tags="${tags} nftstorage"
+    ecosystem="${ecosystem} filecoin ipfs"
 fi
+
 # Print out final list of tags
-echo ${tags}
+echo ${tags} ${ecosystem} | tr " " "\n" | sort | uniq | tr "\n" " "
+echo
